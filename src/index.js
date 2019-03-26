3069596df2231baab947c98dfb5276db898e6505
yyh123 <yuyunhao1@126.com>
2019-03-26 22:24:55 +0800

    上传单个文件

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
-
 const app = express()
+const multer = require('multer')
+const upload = multer({
+  dest: 'uploads/'
+})
 
+/**
+ * 处理单个文件上传
+ * avatar:上传表单中对应的文件名称
+ */
+app.post('/profile', upload.single('avatar'), (req, res, next) => {
+  res.send(req.file)
+})
 
 app.listen(3000, () => {
   global.console.log('localhost:3000')
