cce194cd83c42e23d8d66f0e833cc76d28adb720
yyh123 <yuyunhao1@126.com>
2019-03-26 22:34:03 +0800

    上传多个文件

--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ app.post('/profile', upload.single('avatar'), (req, res, next) => {
   res.send(req.file)
 })
 
+/**
+ * 处理多个文件上传
+ * 指定最多上传3个
+ */
+
+app.post('/photos/upload', upload.array('photos', 3), (req, res, next) => {
+  res.send(req.files)
+})
+
 app.listen(3000, () => {
   global.console.log('localhost:3000')
 })
